33e9c5e3868577face5a583f9e5a273fa02287a6
anishpdm <anish.vilayil.s@gmail.com>
2021-12-04 10:54:12 +0530

    upto 2nd Day

---
+++ app/src/main/java/com/logixspace/mzcapp2021latest/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.logixspace.mzcapp2021latest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
